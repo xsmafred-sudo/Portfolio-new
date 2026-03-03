8a1d3b2bb81cd64c06e8e0ef5897fc80416f1c0f
xsmafred-sudo <xsmafred@gmail.com>
2026-03-03 07:11:21 -0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,6 @@ import { SectionDivider } from '@/components/section-divider';
 import { Services } from '@/components/services';
 import { ThemeToggle } from '@/components/theme-toggle';
 import { FlickeringFooter } from '@/components/ui/flickering-footer';
-import { FlickeringGrid } from '@/components/ui/flickering-grid';
 import { projectsData } from '@/lib/data';
 
 const projectsJsonLd = {
@@ -40,16 +39,6 @@ const HomePage = async () => {
         type="application/ld+json"
         dangerouslySetInnerHTML={{ __html: JSON.stringify(projectsJsonLd) }}
       />
-      <div className="fixed inset-0 -z-10">
-        <FlickeringGrid
-          className="size-full"
-          squareSize={4}
-          gridGap={6}
-          color="#6B7280"
-          maxOpacity={0.3}
-          flickerChance={0.1}
-        />
-      </div>
       <div className="container flex flex-col items-center">
         <Header />
         <Intro />
